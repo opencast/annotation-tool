e4488a7e9b628666945714fbd844f9aca5d3cde8
Rüdiger Rolf <rrolf@rolf.virtuos.uni-osnabrueck.de>
2016-06-14 15:41:42 +0200

    Removed temporary data, added updqated gitignore file

--- opencast-backend/entwine-annotations-tool/target/classes/ui/build/config/require.js.build.js
+++
@@ -1,7 +0,0 @@
-({
-    baseUrl: './../../js',
-    mainConfigFile: './../../js/libs/require/config/config.js',
-    name: "main",
-    out: "../../annotationTool-build.js"
-
-})
--- opencast-backend/entwine-annotations-tool/target/classes/ui/js/libs/backbone/loader.js
+++
@@ -1,8 +0,0 @@
-define(['order!jquery', 'order!underscore', 'order!backbone'],
-function(){
-  return {
-    Backbone: Backbone.noConflict(),
-    _: _.noConflict(),
-    $: jQuery.noConflict()
-  };
-});
