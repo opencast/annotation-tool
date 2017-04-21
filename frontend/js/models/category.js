6372c3f8a5c635b1d246158bc7584badd7b63e53
Julian Kniephoff <kniephoff@elan-ev.de>
2017-04-21 14:49:35 +0200

    Fixed some stylistic issues

--- frontend/js/models/category.js
+++ frontend/js/models/category.js
@@ -1,4 +1,4 @@
-    /**
+/**
  *  Copyright 2012, Entwine GmbH, Switzerland
  *  Licensed under the Educational Community License, Version 2.0
  *  (the "License"); you may not use this file except in compliance
