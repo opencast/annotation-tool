8f3aa36e226104fdabaca7f8ebc3baa267030b97
Julian Kniephoff <kniephoff@elan-ev.de>
2017-05-08 16:34:28 +0200

    Fixed a stylistic issue

--- frontend/Gruntfile.js
+++ frontend/Gruntfile.js
@@ -63,7 +63,7 @@ module.exports = function (grunt) {
             }
         },
 
-        currentProfile: {sources: 'test'},
+        currentProfile: { sources: 'test' },
 
         jshint: {
             all     : '<%= currentWatchFile %>',
