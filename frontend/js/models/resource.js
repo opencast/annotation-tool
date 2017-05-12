742634d7ba6dca0a9c195a9cbe9c3fc04303c1a0
Julian Kniephoff <kniephoff@elan-ev.de>
2017-05-12 11:16:28 +0200

    Introduced a basic resource model

---
+++ frontend/js/models/resource.js
@@ -0,0 +1,37 @@
+/**
+ *  Copyright 2017, ELAN e.V., Germany
+ *  Licensed under the Educational Community License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance
+ *  with the License. You may obtain a copy of the License at
+ *
+ *  http://www.osedu.org/licenses/ECL-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an "AS IS"
+ *  BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ *  or implied. See the License for the specific language governing
+ *  permissions and limitations under the License.
+ *
+ */
+
+/**
+ * A module representing a generic annotation tool resource.
+ * @module models-resource
+ * @requires backbone
+ */
+define(["backbone"], function (Backbone) {
+"use strict";
+
+/**
+ * @constructor
+ * @see {@link http://www.backbonejs.org/#Model}
+ * @augments module:Backbone.Model
+ * @memberOf module:models-resource
+ * @alias module:models-resource.Resource
+ */
+var Resource = Backbone.Model.extend({
+});
+
+return Resource;
+
+});
