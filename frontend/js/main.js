08fd9a8647c8cc5a6a1dc232cf91adb56a89c5b5
Julian Kniephoff <kniephoff@elan-ev.de>
2017-06-30 12:45:37 +0200

    Changed startup to accomodate i18n

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -2,12 +2,27 @@
  * Bootstrap file for require.js
  */
 require(["domReady",
-         "annotations-tool-configuration",
-         "annotations-tool"],
+         "jquery",
+         "i18next",
+         "i18next-xhr-backend",
+         "jquery-i18next",
+         "annotations-tool-configuration"],
 
-        function (domReady, config, app) {
-            domReady(function(){
-                app.start(config);
+    function (domReady, $, i18next, i18nextXHRBackend, $i18next, config) {
+        i18next.use(i18nextXHRBackend)
+            .init({
+                lng: "en",
+                backend: {
+                    loadPath: "locales/{{lng}}/translation.json"
+                }
+            }, function () {
+                $i18next.init(i18next, $, { parseDefaultValueFromContent: false });
+                domReady(function(){
+                    $('[data-i18n]').localize();
+                    require(["annotations-tool"], function (app) {
+                        app.start(config);
+                    });
+                });
             });
-        }
+    }
 );
