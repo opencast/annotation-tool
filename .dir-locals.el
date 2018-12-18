6ac7e717b06e36793eb8c0a4e467a5290f0c617d
Julian Kniephoff <kniephoff@elan-ev.de>
2018-12-18 10:01:56 +0100

    Ignore `node_modules` when using `rgrep`

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,5 +1,8 @@
 ((nil . ((indent-tabs-mode . nil)
          (c-basic-offset . 2)
          (eval . (progn
-                   (c-set-offset 'arglist-cont-nonempty 8)))))
+                   (c-set-offset 'arglist-cont-nonempty 8)
+                   (require 'grep)
+                   (add-to-list (make-local-variable 'grep-find-ignored-directories)
+                                "node_modules")))))
  (js2-mode . ((js2-basic-offset . 4))))
