417141c53f5864a75356580ab24ef3f37c0be9c6
Julian Kniephoff <kniephoff@elan-ev.de>
2019-03-29 15:21:45 +0100

    Only initialize the player if the container is already in the DOM
    MediaElement's fill-mode needs access to the dimensions
    of its parent element in order to adapt its own elements.
    This is impossible when they are not part of the DOM, yet.

--- frontend/js/views/main.js
+++ frontend/js/views/main.js
@@ -380,8 +380,8 @@ define(["jquery",
                 goldenLayout.registerComponent("player", function (container) {
                     self.setLoadingProgress(50, i18next.t("startup.loading video"));
 
-                    container.on("resize", function () {
-                        annotationTool.playerAdapter.resetSize();
+                    container.on("open", function () {
+                        annotationTool.loadVideo(container.getElement()[0]);
                     });
 
                     function videoLoaded() {
@@ -395,8 +395,6 @@ define(["jquery",
                             $(annotationTool.playerAdapter).one(PlayerAdapter.EVENTS.READY, videoLoaded);
                         }
                     });
-
-                    annotationTool.loadVideo(container.getElement()[0]);
                 });
 
                 goldenLayout.registerComponent("timeline", function (container) {
