a94b81406e44b9f0a41fa624a15c30ead470ecf1
Julian Kniephoff <kniephoff@elan-ev.de>
2019-08-19 11:08:37 +0200

    Use timestamp and has as version

--- .travis.env.sh
+++ .travis.env.sh
@@ -1,2 +1,2 @@
 shopt -s expand_aliases
-alias mvn="mvn -DskipTests --batch-mode -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn -Drevision='$TRAVIS_BUILD_NUMBER'"
+alias mvn="mvn -DskipTests --batch-mode -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn -Drevision='$(date +%Y%m%d%H%M%S)-$TRAVIS_COMMIT'"
