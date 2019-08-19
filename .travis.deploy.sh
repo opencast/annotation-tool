dd4db2cb6b37ea25e19d93fdc71b50d878fd5cba
Julian Kniephoff <kniephoff@elan-ev.de>
2019-08-19 12:01:06 +0200

    Fix the build

--- .travis.deploy.sh
+++ .travis.deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 . ./.travis.env.sh
-mvn -s .travis.settings.xml -DskipTests deploy
+mvn -s .travis.settings.xml -DskipTests ${1:-install}
--- .travis.env.sh
+++ .travis.env.sh
@@ -1,2 +1,2 @@
 shopt -s expand_aliases
-alias mvn="mvn -DskipTests --batch-mode -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn -Drevision='$(date +%Y%m%d%H%M%S)-$TRAVIS_COMMIT'"
+alias mvn="mvn -DskipTests --batch-mode -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn -Drevision='$(date +%Y.%m%d.%H%M%S)-$(git rev-parse HEAD)'"
