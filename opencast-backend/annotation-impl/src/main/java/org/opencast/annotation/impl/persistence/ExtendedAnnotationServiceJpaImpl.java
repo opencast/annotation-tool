ab0c4b3607cbf46810994988ffa6fd0509634c20
Julian Kniephoff <kniephoff@elan-ev.de>
2020-06-08 13:27:42 +0200

    Remove an unused import

--- opencast-backend/annotation-impl/src/main/java/org/opencast/annotation/impl/persistence/ExtendedAnnotationServiceJpaImpl.java
+++ opencast-backend/annotation-impl/src/main/java/org/opencast/annotation/impl/persistence/ExtendedAnnotationServiceJpaImpl.java
@@ -83,7 +83,6 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Stream;
 
 import javax.persistence.EntityManager;
 import javax.persistence.RollbackException;
