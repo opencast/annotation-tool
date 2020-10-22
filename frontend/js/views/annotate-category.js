91b40e761633e45b41b67a57f7fabedc7cfeeb75
Arnei <arnepokemon@yahoo.de>
2020-10-22 14:05:07 +0200

    Removed scales from series category
    This is a temporary workaround for the issue with scales not working correctly with series category.
    Only meant as a quick fix since the actual fix would probably take way longer, and should be removed after.

--- frontend/js/views/annotate-category.js
+++ frontend/js/views/annotate-category.js
@@ -172,10 +172,22 @@ define(["jquery",
                     this.model.tmpSeriesCategoryId = categorySeriesCategoryId;
                     this.model.set("seriesExtId", "");
                     this.model.set("seriesCategoryId", "");
+
+                    // Workaround for scales not working
+                    if(this.model.get("scale_id")) {
+                      let settings = _.clone(this.model.get("settings"));
+                      settings.hasScale = true;
+                      this.model.set("settings", settings);
+                    }
                 } else if (!categorySeriesCategoryId && videoSeriesId) {
                     // Add to series
                     this.model.set("seriesExtId", videoSeriesId);
                     this.model.set("seriesCategoryId", this.model.id);
+
+                    // Workaround for scales not working
+                    let settings = _.clone(this.model.get("settings"));
+                    settings.hasScale = false;
+                    this.model.set("settings", settings);
                 }
 
                 this.model.save(null, { wait: true });
