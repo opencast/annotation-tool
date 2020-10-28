c14c4feca457734fc936fd031a900196b0b40e21
Julian Kniephoff <kniephoff@elan-ev.de>
2020-10-28 11:45:15 +0100

    Integration is now hard-coded in the POM

--- .travis.env.sh
+++ .travis.env.sh
@@ -1,2 +1,2 @@
 shopt -s expand_aliases
-alias mvn="mvn -Dintegration=backend -Dmaven.test.skip --batch-mode -Drevision='$TRAVIS_BUILD_NUMBER'"
+alias mvn="mvn -Dmaven.test.skip --batch-mode -Drevision='$TRAVIS_BUILD_NUMBER'"
