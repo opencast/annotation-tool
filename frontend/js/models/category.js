c6de1cd9b31252e940561004848979ad0d672df2
Julian Kniephoff <me@juliankniephoff.com>
2021-01-18 16:48:33 +0100

    Remove an unused parameter `Category#toExportJSON`

--- frontend/js/models/category.js
+++ frontend/js/models/category.js
@@ -201,10 +201,9 @@ define(["underscore",
             /**
              * Prepare the model as JSON to export and return it
              * @alias module:models-category.Category#toExportJSON
-             * @param {boolean} withScales Define if the scale has to be included
              * @return {JSON} JSON representation of the model for export
              */
-            toExportJSON: function (withScale) {
+            toExportJSON: function () {
                 var json = {
                     name: this.attributes.name,
                     labels: this.attributes.labels.map(function (label) {
@@ -230,14 +229,6 @@ define(["underscore",
 
                 if (this.attributes.tags) {
                     json.tags = this.attributes.tags;
-                }
-
-                if (!_.isUndefined(withScale) &&  withScale) {
-                    if (this.attributes.scale_id) {
-                        json.scale = annotationTool.video.get("scales").get(this.attributes.scale_id).toExportJSON();
-                    } else if (this.attributes.scale) {
-                        json.scale = annotationTool.video.get("scales").get(this.attributes.scale.get("id")).toExportJSON();
-                    }
                 }
 
                 return json;
