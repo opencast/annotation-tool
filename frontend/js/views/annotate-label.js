41baf94389bb00b4ab009bbbff75a3f1138a2359
Julian Kniephoff <kniephoff@elan-ev.de>
2021-04-08 13:47:56 +0200

    Also pause for label annotations with scales

--- frontend/js/views/annotate-label.js
+++ frontend/js/views/annotate-label.js
@@ -178,7 +178,7 @@ define(["jquery",
                         scalevalue: scalevalue.toJSON()
                     };
 
-                annotationTool.createAnnotation(params);
+                createAnnotation(params);
             },
 
             /**
