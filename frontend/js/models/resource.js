f866027e0de8a0005a5ee3ba9948029f6c08ed3b
Julian Kniephoff <kniephoff@elan-ev.de>
2021-04-19 08:12:50 +0200

    Make the new sharing option make resources public as well

--- frontend/js/models/resource.js
+++ frontend/js/models/resource.js
@@ -177,7 +177,8 @@ var Resource = Backbone.Model.extend({
      * @alias module:models-resource.Resource#isPublic
      */
     isPublic: function () {
-        return this.get("access") === ACCESS.PUBLIC;
+        var access = this.get("access");
+        return access === ACCESS.PUBLIC || access === ACCESS.SHARED_WITH_EVERYONE;
     },
 
     /**
