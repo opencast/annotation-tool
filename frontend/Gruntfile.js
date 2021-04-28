dff8e1610e06ca34d60634ba00ab811ed71a4f39
Julian Kniephoff <kniephoff@elan-ev.de>
2021-04-28 09:19:31 +0200

    Get rid of the profile shenanigans

--- frontend/Gruntfile.js
+++ frontend/Gruntfile.js
@@ -8,24 +8,13 @@ module.exports = function (grunt) {
 
         /** Paths for the different types of ressource */
         srcPath: 'js/**/*.js',
-
-        profiles: {
-            // Default profile if no one is given
-            default: 'integration',
-
-            integration: {
-                target: '../opencast-backend/annotation-tool/src/main/resources/ui/',
-                integration: `build/integration/${grunt.option('integration') || 'search'}.js`
-            },
-        },
-
-        currentProfile: {},
+        destPath: '../opencast-backend/annotation-tool/src/main/resources/ui/',
 
         clean: {
             options: {
                 force: true
             },
-            target: '<%= currentProfile.target %>'
+            target: '<%= destPath %>'
         },
 
         /** Compile the less files into a CSS file */
@@ -38,7 +27,7 @@ module.exports = function (grunt) {
                 sourceMapURL: 'style/style.css.map',
                 sourceMapBasepath: 'style'
             },
-            '<%= currentProfile.target %>/style/style.css': 'style/style.less'
+            '<%= destPath %>/style/style.css': 'style/style.less'
         },
 
         /** Pre-compile the handlebars templates */
@@ -52,7 +41,7 @@ module.exports = function (grunt) {
                     ext: '.js',
                     expand: true,
                     src: 'templates/*.tmpl',
-                    dest: '<%= currentProfile.target %>'
+                    dest: '<%= destPath %>'
                 }]
             }
         },
@@ -71,13 +60,13 @@ module.exports = function (grunt) {
                         'style/**/*',
                         'locales/**/*.json'
                     ],
-                    dest: '<%= currentProfile.target %>'
+                    dest: '<%= destPath %>'
                 }, {
-                    src: '<%= currentProfile.integration %>',
-                    dest: '<%= currentProfile.target %>/js/annotation-tool-integration.js'
+                    src: `build/integration/${grunt.option('integration') || 'search'}.js`,
+                    dest: '<%= destPath %>/js/annotation-tool-integration.js'
                 }, {
                     src: 'build/config/annotation-tool-configuration.js',
-                    dest: '<%= currentProfile.target %>/js/annotation-tool-configuration.js'
+                    dest: '<%= destPath %>/js/annotation-tool-configuration.js'
                 }]
             }
         },
@@ -103,42 +92,11 @@ module.exports = function (grunt) {
         }
     });
 
-    /**
-     * Set the profile in the configuration but also as a command line option,
-     * so that any child processes like those spawned by the `concurrent` task
-     * can pick it up, too.
-     */
-    function setProfile(profile) {
-        var config = grunt.config.get('profiles')[profile];
-        if (!config) {
-            grunt.fail.fatal('The profile "' + profile + '" does not exist in the Gruntfile.');
-        }
-        grunt.config.set('currentProfile', config);
-        grunt.option('profile', profile);
-    }
-    var profile = grunt.option('profile');
-    if (profile) setProfile(profile);
-
     require('jit-grunt')(grunt);
 
     /** ================================================
      *  Register custom tasks
      ==================================================*/
 
-    grunt.registerTask('baseINTEGRATION', ['clean', 'amdcheck', 'handlebars', 'less', 'copy']);
-
-    grunt.registerTaskWithProfile = function (name, description, profile) {
-        grunt.registerTask(name, description, function () {
-
-            // Configure the tasks with given profiles
-            if (!profile) profile = grunt.config.get("profiles.default");
-            setProfile(profile);
-            grunt.log.writeln(name + ' task with profile "' + profile + '" started! ');
-
-            // Run the tasks
-            grunt.task.run('base' + name.toUpperCase());
-        });
-    };
-
-    grunt.registerTaskWithProfile('integration', 'Deploy webapp in Opencast backend', 'integration');
+    grunt.registerTask('default', ['clean', 'amdcheck', 'handlebars', 'less', 'copy']);
 };
