38ffdc29145b49be189e30e4789d639a4631f5bd
Julian Kniephoff <kniephoff@elan-ev.de>
2021-05-17 11:12:43 +0200

    Put a basic linting setup in place

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    overrides: [{
+        files: ['Gruntfile.js', '.eslintrc.js'],
+        env: {
+            node: true,
+            es2020: true
+        }
+    }, {
+        files: 'js/**/*.js',
+        env: {
+            browser: true
+        }
+    }],
+};
