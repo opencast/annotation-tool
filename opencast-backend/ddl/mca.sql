150b0eae88199f1c154d4910043b5c633e1f125a
Christian Oellers <2101569+ChristianOellers@users.noreply.github.com>
2022-08-04 11:02:50 +0200

    Update migration script

--- opencast-backend/ddl/mca.sql
+++ opencast-backend/ddl/mca.sql
@@ -1,6 +1,9 @@
 -- TODO This is MariaDB-specific. What about other databases?!
 -- TODO Transactions?
 
+-- Might be needed in some cases:
+-- SET SQL_SAFE_UPDATES = 0;
+
 alter table xannotations_annotation
 add content text;
 
@@ -21,8 +24,23 @@ update xannotations_annotation
 set content = concat('[{"type":"scaling","value":{"label":', label_id, ',"scaling":', scale_value_id, '}}]')
 where scale_value_id is not null;
 
+update xannotations_annotation
+set content = '[]'
+where content is null;
+
+alter table xannotations_annotation
+drop CONSTRAINT FK_xannotations_annotation_label_id,
+drop CONSTRAINT FK_xannotations_annotation_scale_value_id;
+
 alter table xannotations_annotation
 modify content text not null default '[]',
 drop text,
 drop label_id,
 drop scale_value_id;
+
+alter table xannotations_label
+add series_label_id bigint(20) DEFAULT NULL
+
+alter table xannotations_category
+add series_category_id bigint(20) DEFAULT NULL,
+add series_extid bigint(20) DEFAULT NULL
