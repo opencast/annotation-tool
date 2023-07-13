e52fa174e5792339b2694013fa98ff55928dd365
Julian Kniephoff <me@juliankniephoff.com>
2023-07-13 18:02:49 +0200

    Update `h2`
    This necessitates quoting some column names.
    
    Also restrict this dependency to the `test` scope.

--- opencast-backend/annotation-impl/src/main/java/org/opencast/annotation/impl/persistence/CategoryDto.java
+++ opencast-backend/annotation-impl/src/main/java/org/opencast/annotation/impl/persistence/CategoryDto.java
@@ -96,7 +96,7 @@ public class CategoryDto extends AbstractResourceDto {
 
   @ElementCollection
   @MapKeyColumn(name = "name")
-  @Column(name = "value")
+  @Column(name = "\"VALUE\"")
   @CollectionTable(name = "xannotations_category_tags", joinColumns = @JoinColumn(name = "category_id"))
   protected Map<String, String> tags = new HashMap<String, String>();
 
